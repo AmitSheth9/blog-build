a1ae3712467b6c1dbac93bb49dd8c2d36314b06e
AmitSheth9 <amit.sheth9@gmail.com>
2021-11-29 13:42:01 -0800

    all state and props working

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -5,9 +5,9 @@ import './Preview.css'
 export default function Preview({ title, subtitle, font, align, text }) {
   return (
     <div className={`preview ${font}`} style={{ textAlign: align }}>
-      <h1> {title}</h1>
-      <h3> {subtitle}</h3>
-      <p>{text}</p>
+      <h1>title {title}</h1>
+      <h3> subtitle {subtitle}</h3>
+      <p>text{text}</p>
     </div>
   )
 }
